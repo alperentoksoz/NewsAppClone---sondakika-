9c863e9c89a143da71712f2a0e902a9d40fd3a7d
aetoksoz <aetoksoz@gmail.com>
2020-06-15 18:49:11 +0300

    FeedController and navigation bar designed

--- NewsAppMVVMv2/Controller/FeedController.swift
+++ NewsAppMVVMv2/Controller/FeedController.swift
@@ -8,6 +8,99 @@
 
 import UIKit
 
+private let reuseIdentifier = "NewsCell"
+
 class FeedController: UICollectionViewController {
+    // MARK: - Properties
     
+    let navigationSettingsButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "ic_settings_24px"), for: .normal)
+        button.setDimensions(height: 40, width: 40)
+        
+        return button
+    }()
+    
+    let navigationSideMenuButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(.checkmark, for: .normal)
+        
+        return button
+    }()
+    
+//    let navigationSideMenuButton: UIButton = {
+//        let button = UIButton(type: .system)
+//
+//        return button
+//    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.register(HomeNewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleSettings() {
+        print("DEBUG: Handle settings")
+        configureNavigationBar()
+    }
+    
+    @objc func handleLetterSize() {
+        print("DEBUG: Handle letter size")
+    }
+    
+    @objc func handleSideMenuShow() {
+        print("DEBUG: Handle show side menu")
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        collectionView.backgroundColor = .systemPink
+        configureNavigationBar()
+        
+    }
+    
+    
+    func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .white
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationItem.title = "Son Dakika"
+        navigationController?.navigationBar.tintColor = .white
+
+        let settingsButton = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_settings_24px"), style: .plain, target: self, action: #selector(handleSettings))
+        let letterSizeButton = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_format_size_24px"), style: .plain, target: self, action: #selector(handleLetterSize))
+        settingsButton.tintColor = .black
+        letterSizeButton.tintColor = .black
+        navigationItem.rightBarButtonItems = [settingsButton,letterSizeButton]
+    
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_menu_24px"), style: .plain, target: self, action: #selector(handleSideMenuShow))
+        navigationItem.leftBarButtonItem?.tintColor = .black
+    }
+    
+}
+
+extension FeedController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! HomeNewCell
+        
+        return cell
+    }
+}
+
+extension FeedController: UICollectionViewDelegateFlowLayout {
+func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    print("DEBUG: \(collectionView.frame.width)")
+    let cgSize = CGSize(width: collectionView.frame.width, height: 200)
+    return cgSize
+}
 }
--- NewsAppMVVMv2/View/HomeNewCell.swift
+++ NewsAppMVVMv2/View/HomeNewCell.swift
@@ -6,3 +6,72 @@
 //  Copyright © 2020 Alperen Toksöz. All rights reserved.
 //
 
+import UIKit
+
+class HomeNewCell: UICollectionViewCell {
+    // MARK: - Properties
+    
+    private let newsImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.borderWidth = 2
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.setDimensions(height: 150, width: 150)
+        imageView.image = #imageLiteral(resourceName: "newsimage")
+        
+        return imageView
+    }()
+    
+    private let timeLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .systemBlue
+        label.numberOfLines = 1
+        label.font = UIFont.systemFont(ofSize: 12,weight: .heavy)
+        label.text = "14:45"
+        
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 14,weight: .medium)
+        label.text = "Haber başlığı Haber başlığı xHaber başlığı Haber başlığı Haber başlığı Haber başlığı Haber başlığı"
+        
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        backgroundColor = .white
+        
+        let stack = UIStackView(arrangedSubviews: [timeLabel,titleLabel])
+        stack.axis = .vertical
+        stack.spacing = 20
+        
+        addSubview(stack)
+        stack.anchor(top:topAnchor,left: leftAnchor,paddingTop: 16,paddingLeft: 16)
+        
+        addSubview(newsImageView)
+        newsImageView.anchor(top:topAnchor,left: stack.rightAnchor,right: rightAnchor,paddingTop: 16, paddingLeft: 16,paddingRight: 16)
+        
+        
+        
+    }
+}
